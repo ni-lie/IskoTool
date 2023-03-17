1fd8bf71397997b926b9e254b060131d51f3fde5
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-03-17 11:30:38 +0800

    refactor: create dashboard date/time display component

--- IskoTool/src/App.svelte
+++ IskoTool/src/App.svelte
@@ -1,38 +1,18 @@
 <script>
-  let date = new Date();
+  // Import routes
 
-  setInterval(() => {
-    date = new Date();
-  }, 1000);
-
-  $: dayFmt = new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date);
-  $: dateFmt = new Intl.DateTimeFormat('en-US', {month: 'long', day: '2-digit', year: 'numeric'}).format(date);
-  $: timeFmt = new Intl.DateTimeFormat('en-US', {hour: 'numeric', minute: 'numeric'}).format(date);
+  // Import components
+  import DateTimeDisplay from './lib/dashboard components/DashboardDateTimeDisplay.svelte'
 </script>
 
 <main>
-  <h1 class="title">
-    <p class="small-green-text"> Today is </p>
-    {dayFmt} | {dateFmt}
-    <p class="bold-green-text"> {timeFmt} </p>
-  </h1>
+  <!-- Date and Time Display -->
+  <DateTimeDisplay />
+  
+  <!-- Routes -->
+  
   <!-- <PomodoroTimer /> -->
 </main>
 
 <style>
-p {
-  margin: 0;
-}
-
-.small-green-text {
-  color: var(--evergreen-dark);
-  font-size: 1.5rem;
-  font-weight: 300;
-}
-
-.bold-green-text {
-  color: var(--evergreen-dark);
-  font-size: 2rem;
-}
-
 </style>
