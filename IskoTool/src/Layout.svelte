c77c254386d9957daffe4056618dd1492934dd70
ni-lie <cjay.coronel25@gmail.com>
2023-03-20 14:52:07 +0800

    Added layout for notes app

---
+++ IskoTool/src/Layout.svelte
@@ -0,0 +1,19 @@
+<main>
+  <header>
+  </header>
+	<section>
+    <slot></slot>
+	</section>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+    	height: 100vh;
+	}
+	
+	section {
+    	flex-grow: 1;
+	}
+</style>
