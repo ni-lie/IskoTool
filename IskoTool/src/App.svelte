742ddaae784103da5706feae43be46033da605b3
ni-lie <cjay.coronel25@gmail.com>
2023-03-20 14:48:32 +0800

    Made two temporary buttons for navigating through components: (1) Pomodoro Timer (2) Notes

--- IskoTool/src/App.svelte
+++ IskoTool/src/App.svelte
@@ -1,14 +1,22 @@
 <script>
   let title = "POMODORO";
   import PomodoroTimer from './lib/PomodoroTimer.svelte';
+
+  import Layout from './Layout.svelte';
+	import Notes from './Notes.svelte';
+
+  let component = PomodoroTimer;
+
 </script>
 
-<main>
-  <h1 class="title">
-    Pomodoro Timer
-  </h1>
-  <PomodoroTimer />
-</main>
+<nav>
+  <button on:click={() => component = PomodoroTimer}>Pomodoro Timer</button>
+  <button on:click={() => component = Notes}>Notes</button>
+</nav>
+
+<Layout>
+  <svelte:component this={component} />
+</Layout>
 
 <style>
 
