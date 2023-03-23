87f1549a33cf80fea8b85c8bf36f00f69c2d925f
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-03-24 01:33:34 +0800

    feat: include link to notes page in navigation bar

---
+++ IskoTool/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  // Import routes
+  import Router, {location, link} from 'svelte-spa-router';
+  import Dashboard from './pages/Dashboard.svelte';
+  import Pomodoro from './pages/PomodoroPage.svelte';
+  import Notes from './pages/Notes.svelte'; 
+
+  const routes = {
+    '/': Dashboard,
+    '/pomodoro': Pomodoro,
+    '/notes': Notes
+  }
+  
+</script>
+
+<main>
+  <nav>
+    <button><a href="/">Home</a></button>
+    <button><a href="#/pomodoro">Pomodoro</a></button>
+    <button><a href="#/notes">Notes</a></button>
+  </nav>
+
+  <Router {routes} />
+</main>
+
+<style>
+  a {
+    text-decoration: none;
+    color: black;
+  }
+</style>
