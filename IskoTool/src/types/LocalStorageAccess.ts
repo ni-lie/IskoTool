40ff45c1310ca4418d744177a98f8c1b30977eb6
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-03-31 17:16:22 +0800

    refactor: implement helper functions for saving/loading to localStorage

--- IskoTool/src/types/LocalStorageAccess.ts
+++ IskoTool/src/types/LocalStorageAccess.ts
@@ -1,7 +1,28 @@
-export function saveToLocal(key: string, value: any[]) {
-    return localStorage.setItem(key, JSON.stringify(value));
+import type { Note } from '../types/note';
+import type { CustomMode } from './CustomTimerMode';
+
+// export function saveToLocal(key: string, value: any[]) {
+//     return localStorage.setItem(key, JSON.stringify(value));
+// }
+
+// export function loadFromLocal(key: string) {
+//     return JSON.parse(localStorage.getItem(key));
+// }
+
+export function loadCustomModes(): CustomMode[] {
+    const value = localStorage.getItem('pomodoroCustomModes');
+    return value === null ? [] : JSON.parse(value);
 }
 
-export function loadFromLocal(key: string) {
-    return JSON.parse(localStorage.getItem(key));
+export function setCustomModes(customModes: CustomMode[]) {
+    localStorage.setItem('pomodoroCustomModes', JSON.stringify(customModes));
+}
+
+export function loadNotes(): Note[] {
+    const value = localStorage.getItem('userNotes');
+    return value === null ? [] : JSON.parse(value);
+}
+
+export function setNotes(notes: Note[]) {
+    localStorage.setItem('userNotes', JSON.stringify(notes));
 }
