e5f4988b20733d1629b32251c4c2f2032d84c4a6
ni-lie <cjay.coronel25@gmail.com>
2023-04-08 23:37:31 +0800

    Initial commit

---
+++ IskoTool/src/pages/newNotes.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Modal from '../lib/newnotes/modal.svelte';
+
+    let showModal = false;
+
+    const toggleNote = () => {
+        showModal = !showModal;
+    };
+</script>
+
+<Modal message = "Hey there!" {showModal} on:click={toggleNote} />
+<main>
+    <button on:click={toggleNote}>Add a note</button>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+</style>
