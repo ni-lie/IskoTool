f2bba5ce70981e3b2d39d92873e8867f8fb37537
ni-lie <cjay.coronel25@gmail.com>
2023-04-09 09:59:56 +0800

    refactor: transfer form to addnoteForm.svelte

--- IskoTool/src/pages/newNotes.svelte
+++ IskoTool/src/pages/newNotes.svelte
@@ -19,14 +19,7 @@
 </script>
 
 <Modal {showModal} on:click={toggleNote}>
-    <h3> Add a new note </h3>
-    <form>
-        <input type="text" placeholder="Title">
-        <textarea placeholder="Type your note" cols="30" rows="10"></textarea>
-        <br>
-        <button>Save note</button>
-        <button>Delete</button>
-    </form>
+
 </Modal>
 
 <main>
