9fde6e92d4db7152c28e7e7cc05f55b832002f8c
ni-lie <cjay.coronel25@gmail.com>
2023-04-09 12:16:33 +0800

    feat: added some stylings to button

--- IskoTool/src/lib/newnotes/Button.svelte
+++ IskoTool/src/lib/newnotes/Button.svelte
@@ -7,5 +7,12 @@
 </button>
 
 <style>
-
+    button{
+        border: 0;
+        cursor: pointer;
+        border-radius: 6px;
+        padding: 8px 12px;
+        font-weight: bold;
+        box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+    };
 </style>
