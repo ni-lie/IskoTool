372b985c86732308f1a42a4930b5c74155738c08
ni-lie <cjay.coronel25@gmail.com>
2023-04-09 16:06:37 +0800

    feat: added some stylings

--- IskoTool/src/lib/newnotes/notesDetails.svelte
+++ IskoTool/src/lib/newnotes/notesDetails.svelte
@@ -5,7 +5,7 @@
 
 <div class="note">
     <div class="title"> 
-        <h3>{note.title}</h3>
+        <h4>{note.title}</h4>
     </div>
     <div class="note-content">
         <p>{note.noteContent}</p>
@@ -16,7 +16,27 @@
 </div>
 
 <style>
-    h3 {
+    h4 {
+        margin: 0 auto;
         color: #1c8d76;
     }
+    p{
+    margin-top: 6px;
+    font-size: 14px;
+    color: #000000;
+    margin-bottom: 30px;
+    }
+    .title, .note-content{
+        background: #fafafa;
+        cursor: pointer;
+        margin: 10px auto;
+        position: relative;
+    }
+    .title:hover, .note-content:hover{
+    opacity: 0.6;
+    }
+    /* span{
+        display: inline-block;
+        padding: 10px 20px;
+    } */
 </style>
