3d940220081fe68560ee0bb2a2ba5dc951f725b7
ni-lie <cjay.coronel25@gmail.com>
2023-04-09 14:07:41 +0800

    refactor: transferred the display of notes to notesList component

--- IskoTool/src/pages/newNotes.svelte
+++ IskoTool/src/pages/newNotes.svelte
@@ -32,16 +32,7 @@
 
 <main>
     <button on:click={toggleNote}>Add a note</button>
-
-    {#each notes as note (note.id)}
-        <div>
-            <h3>{note.title}</h3>
-            <p>{note.noteContent}</p>
-            <button on:click={() => {handleDelete(note.id)}}>Delete</button>
-        </div>
-    {:else}
-        <p>There are no notes to show...</p>
-    {/each}
+    <NotesList notes={notes} />
     
 </main>
 
