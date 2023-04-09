018b1456158f3c665746d391bacb076c3f99fd46
ni-lie <cjay.coronel25@gmail.com>
2023-04-09 10:13:46 +0800

    feat: added submit method, an event modifier -- preventDefault, and a handleSubmit func

--- IskoTool/src/lib/newnotes/addnoteForm.svelte
+++ IskoTool/src/lib/newnotes/addnoteForm.svelte
@@ -1,16 +1,21 @@
 <script>
     let title;
     let noteMessage;
+
+    const handleSubmit = () => {
+        console.log(title, noteMessage);
+    };
 </script>
 
 <h3> Add a new note </h3>
-    <form>
-        <input type="text" placeholder="Title" bind:value={title}>
-        <textarea placeholder= "Type your note" cols="30" rows="10" bind:value={noteMessage}></textarea>
-        <br>
-        <button>Save note</button>
-        <button>Delete</button>
-    </form>
+<form on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="Title" bind:value={title}>
+    <textarea placeholder= "Type your note" cols="30" rows="10" bind:value={noteMessage}></textarea>
+    <br>
+    <button>Save note</button>
+    <button>Delete</button>
+</form>
+
 <style>
     input {
         background-color: white;
