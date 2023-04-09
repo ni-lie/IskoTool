dc035ac4d24bc79fedda6f3f3398a4f2b02a810c
ni-lie <cjay.coronel25@gmail.com>
2023-04-09 15:59:01 +0800

    feat: created divs for title and noteContent

--- IskoTool/src/lib/newnotes/notesDetails.svelte
+++ IskoTool/src/lib/newnotes/notesDetails.svelte
@@ -4,8 +4,12 @@
 
 
 <div class="note">
-    <h3>{note.title}</h3>
-    <p>{note.noteContent}</p>
+    <div class="title"> 
+        <h3>{note.title}</h3>
+    </div>
+    <div class="note-content">
+        <p>{note.noteContent}</p>
+    </div>
     <!-- <Button type="secondary" on:click={() => {handleDelete(note.id)}}> Delete </Button> -->
     <!-- <DeletenoteForm on:deleteNote={note.id}/> -->
     <!-- <DeletenoteForm noteID={note.id} notes={notes}/> -->
