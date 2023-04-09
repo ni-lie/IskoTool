7973431a12b9ccafc5ff40c9cad785d6784420bd
ni-lie <cjay.coronel25@gmail.com>
2023-04-09 10:00:23 +0800

    refactor: transferred form from newNotes.svelte

--- IskoTool/src/lib/newnotes/addnoteForm.svelte
+++ IskoTool/src/lib/newnotes/addnoteForm.svelte
@@ -2,10 +2,14 @@
 
 </script>
 
-<form>
-
-</form>
-
+<h3> Add a new note </h3>
+    <form>
+        <input type="text" placeholder="Title">
+        <textarea placeholder="Type your note" cols="30" rows="10"></textarea>
+        <br>
+        <button>Save note</button>
+        <button>Delete</button>
+    </form>
 <style>
-    
+
 </style>
