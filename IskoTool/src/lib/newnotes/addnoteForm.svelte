ccb8c1fc98c7f9d3204bb252c70da76da7e5e4cc
ni-lie <cjay.coronel25@gmail.com>
2023-04-09 10:05:04 +0800

    feat: added bindings to title and note content

--- IskoTool/src/lib/newnotes/addnoteForm.svelte
+++ IskoTool/src/lib/newnotes/addnoteForm.svelte
@@ -1,11 +1,12 @@
 <script>
-
+    let title;
+    let noteMessage;
 </script>
 
 <h3> Add a new note </h3>
     <form>
-        <input type="text" placeholder="Title">
-        <textarea placeholder="Type your note" cols="30" rows="10"></textarea>
+        <input type="text" placeholder="Title" bind:value={title}>
+        <textarea placeholder= "Type your note" cols="30" rows="10" bind:value={noteMessage}></textarea>
         <br>
         <button>Save note</button>
         <button>Delete</button>
