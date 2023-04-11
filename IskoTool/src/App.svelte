f09205ddce8dfc2aae770e63f54a1d1a19fba274
igsanceda <igsanceda@up.edu.ph>
2023-04-11 19:25:45 +0800

    feat: added calendar

--- IskoTool/src/App.svelte
+++ IskoTool/src/App.svelte
@@ -4,9 +4,11 @@
   import Dashboard from './pages/Dashboard.svelte';
   import Pomodoro from './pages/PomodoroPage.svelte';
   import Notes from './pages/Notes.svelte'; 
+  import Calendar from './pages/CalendarPage.svelte'
 
   const routes = {
     '/': Dashboard,
+    '/calendar': Calendar,
     '/pomodoro': Pomodoro,
     '/notes': Notes
   }
@@ -16,6 +18,7 @@
 <main>
   <nav>
     <button><a href="/">Home</a></button>
+    <button><a href="#/calendar">Calendar</a></button>
     <button><a href="#/pomodoro">Pomodoro</a></button>
     <button><a href="#/notes">Notes</a></button>
   </nav>
---
+++ IskoTool/src/pages/CalendarPage.svelte
@@ -0,0 +1,8 @@
+<script>
+    let title = "CALENDAR";
+	import Calendar from '../lib/calendar/Calendar.svelte';
+</script>
+
+<main>
+    <Calendar />
+</main>
