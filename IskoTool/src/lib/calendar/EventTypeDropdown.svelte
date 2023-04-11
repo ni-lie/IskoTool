4fbbb349d4571ec9f364e8e7a38e07dd00598a32
igsanceda <igsanceda@up.edu.ph>
2023-04-11 21:21:44 +0800

    feat: added dropdown for selecting event type

---
+++ IskoTool/src/lib/calendar/EventTypeDropdown.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { mode } from './PomodoroModeChoice';
+
+    let select: HTMLSelectElement;
+    function onChange() {
+        mode.set(select.selectedIndex);
+    }
+</script>
+
+<select bind:this={select} on:change={onChange}>
+    <option>Pomodoro</option>
+    <option>Short Break</option>
+    <option>Long Break</option>
+    <!-- custom mode choice -->
+</select>
+
+<style>
+select {
+    appearance: none;
+    text-align: center;
+    color: var(--evergreen-dark);
+    font-family: 'Space Grotesk';
+    font-size: 20px;
+    font-weight: bold;
+    width: 180px;
+    padding: 7px 0px;
+    margin: 20px 20px 20px 2rem;
+    border-radius: 24px;
+    border-width: 3px;
+    border-style: solid;
+    border-color: white;
+    background-color: white;
+  }
+
+</style>
