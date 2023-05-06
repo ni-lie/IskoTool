226a31b4c09ea88c1d5be7a7e5483cdfbaeb91a6
igsanceda <igsanceda@up.edu.ph>
2023-05-06 12:31:08 +0800

    refactor: removed adding events from event component

--- src/renderer/src/lib/calendar/Events.svelte
+++ src/renderer/src/lib/calendar/Events.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
     import { eventStore } from './CalendarStore';
     import type { Event } from '../../types/event';
-    import AddEventModal from './AddEventModal.svelte';
     import EditEventModal from './EditEventModal.svelte';
     import Svelecte from 'svelecte';
 
-    let showAddEventModal = false;
     let showEditEventModal = false;
     let selectedID: string | null = null;
     let event: Event = {
@@ -21,10 +19,6 @@
 		if (found) event = found;
 	}
 
-    function addEvent(e) {
-        eventStore.addEvent(e.detail);
-    }
-
     function editEvent(e) {
         eventStore.saveEvent(e.detail);
     }
@@ -39,8 +33,6 @@
 
 <section>
     <Svelecte options={$eventStore} bind:value={selectedID} valueField="id" labelField="name" placeholder="Select (or search for) an event..."></Svelecte>
-    <button class="fadedtext" on:click={() => (showAddEventModal = true)}>+ New Event</button>
-    <AddEventModal bind:showAddEventModal on:addNewEvent={addEvent} />
     <button class="fadedtext" disabled={selectedID === null} on:click={() => (showEditEventModal = true)}>Edit Event</button>
     <EditEventModal bind:showEditEventModal bind:event on:editExistingEvent={editEvent} />
     <button disabled={selectedID === null} on:click={deleteEvent}>Delete</button>
