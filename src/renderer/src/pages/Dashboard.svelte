1e89ccd624a0a6b18c85a0eb75efc5e2007c1757
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-05-12 06:05:10 +0800

    feat: add dashboard buttons to navigate to notes and pomodoro

--- src/renderer/src/pages/Dashboard.svelte
+++ src/renderer/src/pages/Dashboard.svelte
@@ -5,12 +5,45 @@
 </script>
   
 <main>
-    <!-- Date and Time Display -->
-    <DateTimeDisplay />
-    <DailyEventsDisplay />
-    <MonthlyEventsDisplay />
-
+    <section>
+        <DateTimeDisplay />
+        <div class="display-box">
+            <DailyEventsDisplay />
+            <MonthlyEventsDisplay />
+        </div>
+        <div class="button-group">
+            <a href="#/pomodoro">Start a Pomodoro session</a>
+            <a href="#/notes">View your notes</a>
+        </div>
+    </section>
 </main>
   
 <style>
+    .display-box {
+        display: flex;
+        margin: 0px;
+        justify-content: space-evenly;
+        max-height: 30rem;
+    }
+
+    .button-group {
+        margin: 2em 5em 0 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        float: right;
+    }
+
+    a {
+    text-align: center;
+    background-color: var(--evergreen-dark);
+    color: white;
+    font-family: 'Rubik';
+    font-weight: bold;
+    width: 250px;
+    padding: 12px 0px;
+    margin: 4px;
+    border-radius: 12px;
+    }
+
 </style>
