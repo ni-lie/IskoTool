bbc7e7431063579103891f2f0a5ef7f179b3d4ac
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-05-24 05:21:09 +0800

    refactor: apply custom dropdown styles to Month picker

--- src/renderer/src/lib/calendar/EventTypeDropdown.svelte
+++ src/renderer/src/lib/calendar/EventTypeDropdown.svelte
@@ -14,51 +14,12 @@
 	}
 </script>
 
-<div class="select-holder">
+<span class="select-holder">
     <select bind:value={select} on:click={toggleChoiceList} on:blur={setInactive}>
         <option value="General">General</option>
         <option value="Birthday">Birthday</option>
         <option value="Holiday">Holiday</option>
         <option value="Appointment">Appointment</option>
     </select>
-    <img class="arrow" class:expanded={active} src={dropdownArrow} alt='ff'>
-</div>
-
-<style>
-
-div.select-holder {
-    min-width: 286px;
-    min-height: 0;
-    margin-bottom: 10px;
-    display: flex;
-    position: relative;
-    align-items: center;
-}
-
-select {
-    appearance: none;
-    text-align: left;
-    color: black;
-    font-family: 'Space Grotesk';
-    font-size: 15px;
-    padding: 7px 10px;
-    width: 60%;
-    border: solid var(--evergreen-dark) 1px;
-    border-radius: 3px;
-    background-color: white;
-  }
-
-.expanded {
-		transform: rotate(90deg);
-    }
-
-.arrow {
-    position: absolute;
-    left: 55%;
-    top: 35%;
-    width: 4%;
-    vertical-align: middle;
-    pointer-events: none;
-    transition: 0.2s;
-}
-</style>
+    <img class="arrow" class:expanded={active} src={dropdownArrow} alt='Dropdown arrow'>
+</span>
