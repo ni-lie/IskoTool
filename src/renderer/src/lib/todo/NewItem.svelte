2e9590767d3d3a6ef1638b6399947933238bf2a3
ni-lie <cjay.coronel25@gmail.com>
2023-05-25 11:48:41 +0800

    import: createEventDispatcher

--- src/renderer/src/lib/todo/NewItem.svelte
+++ src/renderer/src/lib/todo/NewItem.svelte
@@ -1,5 +1,10 @@
 <script>
-
+    import { createEventDispatcher } from "svelte";
+    
+    function handleKeyUp({key}){
+        if (key !== 'Enter' || value.length === 0) return;
+    }
+    let value = "";
 </script>
 
-<input type = "text" on:keyup = {handleKeyUp} placeholder = "+ Add Item">
+<input type = "text" on:keyup = {handleKeyUp} bind:value placeholder = "+ Add Item">
