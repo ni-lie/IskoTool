ef922569f8f5aaf210f5fa9064b1e19fe67f2f67
ni-lie <cjay.coronel25@gmail.com>
2023-05-25 10:29:39 +0800

    populated some temporary list

--- src/renderer/src/lib/todo/stores.js
+++ src/renderer/src/lib/todo/stores.js
@@ -1,4 +1,15 @@
 import { writable } from "svelte/store";
 
-export const items = writable([]);
+export const items = writable([
+    {
+        id: 1,
+        text: "Do cs 145 labex",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Do cs 155 labex",
+        completed: true
+    },
+]);
 
