532cc87697c011cc19db4fd4c05e81728273ef3b
ni-lie <cjay.coronel25@gmail.com>
2023-05-25 10:04:44 +0800

    added temporary text and stylings

---
+++ src/renderer/src/lib/todo/Todo.svelte
@@ -0,0 +1,18 @@
+<script>
+	
+</script>
+
+<div class="container">
+    sample text
+</div>
+
+<style>
+    .container {
+        width: 500px;
+        background: #009579;
+    }
+
+    :global(.container *) {
+        font-family:sans-serif
+    }
+</style>
