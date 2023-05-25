d08d26fe18d9e3d80b6dbd72efe0e1fb5d1e77e6
ni-lie <cjay.coronel25@gmail.com>
2023-05-25 10:38:58 +0800

    feat: added three functions; to be edited later on

--- src/renderer/src/lib/todo/List.svelte
+++ src/renderer/src/lib/todo/List.svelte
@@ -1,1 +1,19 @@
-I am the list
+<script>
+    import { onMount } from "svelte";
+
+    function handleNewItem(e) {
+
+    }
+
+    function handleUpdate(e) {
+        
+    }
+
+    function handleDelete(e) {
+        
+    }
+
+    onMount(async () => {
+        // fetch from API
+    });
+</script>
