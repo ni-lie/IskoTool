f6a70f1e9d9e07a7c2b6349f737892e8980985c5
ni-lie <cjay.coronel25@gmail.com>
2023-05-25 11:54:11 +0800

    added some styling to input

--- src/renderer/src/lib/todo/NewItem.svelte
+++ src/renderer/src/lib/todo/NewItem.svelte
@@ -12,3 +12,19 @@
 </script>
 
 <input type = "text" on:keyup = {handleKeyUp} bind:value placeholder = "+ Add Item">
+
+<style>
+    input {
+        box-sizing: border-box;
+        margin-bottom: 15px;
+        padding: 15px;
+        border: none;
+        outline: none;
+        width: 100%;
+        background: rgba(0, 0, 0, 0.25) ;
+        color: white;
+        font-weight: bold;
+        font-size: 1em;
+        cursor: pointer;
+    }
+</style>
