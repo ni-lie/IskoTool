917123c24c035b62236424b3849c421c0f4d40d2
ni-lie <cjay.coronel25@gmail.com>
2023-05-25 10:05:04 +0800

    build: header

---
+++ src/renderer/src/lib/todo/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<div class = "header">
+    <span> To-Do List</span>
+    <span>0/0</span>
+</div>
+
+<style>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 15px 20 px;
+        color: white;
+        font-weight: bold;
+        font-size: 1.5em;
+        background: rgba(0, 0, 0, 0.1);
+    }
+</style>
