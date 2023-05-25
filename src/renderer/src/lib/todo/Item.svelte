bc042686aaac4c87ab6fdc6bae974d4903a2221f
ni-lie <cjay.coronel25@gmail.com>
2023-05-25 10:54:08 +0800

    build: item.svelte

---
+++ src/renderer/src/lib/todo/Item.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let id, text, completed;
+</script>
+
+<div class="item">
+    <input class="text-input" type="text" bind:value={text} readonly={completed} /> 
+    <input class="completed-checkbox" type="checkbox" bind:checked={completed} />
+</div>
+
+<style>
+
+</style>
