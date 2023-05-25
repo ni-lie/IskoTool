af90e1eb8407d1d8b1861ea1487284234a2dcd95
ni-lie <cjay.coronel25@gmail.com>
2023-05-25 12:19:25 +0800

    deleted temporary stores

--- src/renderer/src/lib/todo/stores.js
+++ src/renderer/src/lib/todo/stores.js
@@ -1,15 +1,5 @@
 import { writable } from "svelte/store";
 
 export const items = writable([
-    {
-        id: 1,
-        text: "Do cs 145 labex",
-        completed: false
-    },
-    {
-        id: 2,
-        text: "Do cs 155 labex",
-        completed: true
-    },
 ]);
 
